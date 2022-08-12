1c36eeee43d777e951476092c5b88f41bf0ce9fc
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-08-12 09:46:02 +0200

    Add Baner

--- src/components/layouts/MainLayout/MainLayout.js
+++ src/components/layouts/MainLayout/MainLayout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Baner from '../../view/Baner/Baner';
 import NavBar from '../../view/NavBar/NavBar';
 
 const MainLayout = () => {
   return (
     <div>
       <NavBar />
+      <Baner />
       <h1>MainLayout</h1>
     </div>
   )
--- src/components/view/Baner/Baner.js
+++ src/components/view/Baner/Baner.js
@@ -1,10 +1,24 @@
+import { Carousel } from 'react-bootstrap';
 import React from 'react';
 import styles from './Baner.module.scss';
 
 const Baner = () => {
+
+  const images = ['baner1.jpg', 'baner2.jpg', 'baner3.jpg', 'baner4.jpg', 'baner5.jpg'];
+
   return (
     <div className={styles.container}>
-      baner
+      <Carousel variant='dark' interval={1000} fade={true} indicators={false} controls={false}>
+        {images.map(image => 
+        <Carousel.Item key={image}>
+          <img
+            className='d-block w-100'
+            src={'/img/baner/'+ image}
+            alt={image}
+          />
+        </Carousel.Item>
+        )}
+      </Carousel>
     </div>
   )
 }
